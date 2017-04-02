d585e61d50781f3c76cac00a3e6555cc530f6257
Evan Koschik <ekoschik@umich.edu>
2017-04-01 23:51:49 -0700

    Cleanup

--- MouseSpeedDisplay/HookCoordinates/MouseSpeedDisplay.cpp
+++ MouseSpeedDisplay/HookCoordinates/MouseSpeedDisplay.cpp
@@ -12,7 +12,7 @@ HWND hwnd;
 
 // Default window size
 int defCX = 600, defCY = 400;
-int defX = 500, defY = 500;
+int defX = 0, defY = 0;
 
 // Defines color range, and 'top' speed (100% speed)
 const double goal = 300;
@@ -44,63 +44,6 @@ COLORREF GetAvgSpeedColor(int percentage)
                CLRMATH((double)GetBValue(c1), (double)GetBValue(c2), fraction));
 }
 
-void PrintText(HDC hdc, RECT rcClient, int percentage)
-{
-    WCHAR buf[100];
-#define STR (LPWSTR)&buf
-#define PRINT_TEXT(prc, DT_flags, ...) \
-    wsprintf(STR, __VA_ARGS__); \
-    DrawText(hdc, STR, wcslen(STR), prc, DT_flags);
-#define PRINT_CENTER(prc, ...) \
-    PRINT_TEXT(prc, DT_CENTER | DT_VCENTER | DT_SINGLELINE, __VA_ARGS__)
-#define PRINT_LEFT(prc, ...) PRINT_TEXT(prc, DT_LEFT, __VA_ARGS__)
-
-    SetBkMode(hdc, TRANSPARENT);
-
-    static HFONT hfontLarge = CreateFont(
-        45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, L"TimesNewRoman");
-    static HFONT hfontSmall = CreateFont(
-        20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, L"TimesNewRoman");
-    
-    // Large text in the center for percentage
-
-    SelectFont(hdc, hfontLarge);
-    RECT rcTxt = rcClient;
-    PRINT_CENTER(&rcTxt, L"%i%%", percentage);
-    
-    // Small text in bottom right for avg speed and distance
-
-    SelectFont(hdc, hfontSmall);
-
-    int inc = 20;
-    rcTxt.top = rcTxt.bottom - (3 * inc);
-    rcTxt.left += inc;
-
-    PRINT_LEFT(&rcTxt, L"average speed: %i", averageSpeed);
-    rcTxt.top += inc;
-    const int distDivBy = 1000;
-    PRINT_LEFT(&rcTxt, L"total distance: %i (x %i px)",
-        (int)(distance / distDivBy), distDivBy);
-    
-    // Close button
-
-    SetRect(&rcTxt, rcClient.right - 80,
-        rcClient.top + 8,
-        rcClient.right - 8,
-        rcClient.top + 8 + 25);
-    CopyRect(&rcCloseButton, &rcTxt);
-    
-    if (bHoverCloseButton) {
-        // Pick a number 30 away from the current percentage to get a sufficiently
-        // different color for the close button compared to the background.
-        HBRUSH hbrCloseHover = CreateSolidBrush(
-            GetAvgSpeedColor((percentage + 30) % 100));
-        FillRect(hdc, &rcTxt, hbrCloseHover);
-    }
-
-    PRINT_CENTER(&rcTxt, L"close");
-}
-
 void Draw(HDC hdc, HWND hwnd)
 {
     RECT rcClient = {};
@@ -113,7 +56,61 @@ void Draw(HDC hdc, HWND hwnd)
     HBRUSH hbr = CreateSolidBrush(GetAvgSpeedColor(percentage));
     FillRect(hdc, &rcClient, hbr);
 
-    PrintText(hdc, rcClient, percentage);
+    WCHAR buf[100];
+#define STR (LPWSTR)&buf
+#define PRINT_TEXT(prc, DT_flags, ...) \
+    wsprintf(STR, __VA_ARGS__); \
+    DrawText(hdc, STR, wcslen(STR), prc, DT_flags);
+#define PRINT_CENTER(prc, ...) \
+    PRINT_TEXT(prc, DT_CENTER | DT_VCENTER | DT_SINGLELINE, __VA_ARGS__)
+#define PRINT_LEFT(prc, ...) PRINT_TEXT(prc, DT_LEFT, __VA_ARGS__)
+
+    SetBkMode(hdc, TRANSPARENT);
+
+    static HFONT hfontLarge = CreateFont(
+        45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, L"Arial");
+    static HFONT hfontSmall = CreateFont(
+        20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, L"Arial");
+
+    // Large text in the center for percentage
+
+    SelectFont(hdc, hfontLarge);
+    RECT rcTxt = rcClient;
+    PRINT_CENTER(&rcTxt, L"%i%%", percentage);
+
+    // Small text in bottom left for avg speed and distance
+
+    SelectFont(hdc, hfontSmall);
+
+    int inc = 20;
+    rcTxt.top = rcTxt.bottom - (3 * inc);
+    rcTxt.left += inc;
+
+    PRINT_LEFT(&rcTxt, L"average speed: %i, top speed: %i",
+        averageSpeed, (int)maxDelta);
+    rcTxt.top += inc;
+    const int distDivBy = 1000;
+    PRINT_LEFT(&rcTxt, L"total distance: %i (x%i px)",
+        (int)(distance / distDivBy), distDivBy);
+
+    // Close button
+
+    int width = 28, height = 23, textbuf = 8;
+    SetRect(&rcTxt, rcClient.right - width,
+        rcClient.top + textbuf,
+        rcClient.right - textbuf,
+        rcClient.top + textbuf + height);
+    CopyRect(&rcCloseButton, &rcTxt); // Update rcCloseButton for hit test
+
+    if (bHoverCloseButton) {
+        // Pick a number 30 away from the current percentage to get a sufficiently
+        // different color for the close button compared to the background.
+        HBRUSH hbrCloseHover = CreateSolidBrush(
+            GetAvgSpeedColor((percentage + 30) % 100));
+        FillRect(hdc, &rcTxt, hbrCloseHover);
+    }
+
+    PRINT_CENTER(&rcTxt, L"X");
 }
 
 __inline VOID ResizeRectAroundPoint(PRECT prc, UINT cx, UINT cy, POINT pt)
@@ -126,6 +123,10 @@ __inline VOID ResizeRectAroundPoint(PRECT prc, UINT cx, UINT cy, POINT pt)
 
 void GrowShrink(int delta, POINT ptCursor, bool control, bool shift)
 {
+    if (!control && !shift) {
+        return;
+    }
+
     RECT rcWindow = {};
     GetWindowRect(hwnd, &rcWindow);
 
@@ -189,21 +190,20 @@ LRESULT CALLBACK WndProc(
 
     // Scrolling + control/shift adjust the window's width/height
     case WM_MOUSEWHEEL:
-        if (GET_KEYSTATE_WPARAM(wParam) & MK_CONTROL ||
-            GET_KEYSTATE_WPARAM(wParam) & MK_SHIFT) {
-            static int accum = 0;
-            accum += GET_WHEEL_DELTA_WPARAM(wParam);
-            if (abs(accum) >= WHEEL_DELTA) {
-                POINT ptCursor = { GET_X_LPARAM(lParam) , GET_Y_LPARAM(lParam) };
-                int speed = 4;
-                GrowShrink((accum > 0) ? speed : -speed, ptCursor,
-                    !!(GET_KEYSTATE_WPARAM(wParam) & MK_CONTROL),
-                    !!(GET_KEYSTATE_WPARAM(wParam) & MK_SHIFT));
-                accum = 0;
-            }
+    {
+        static int accum = 0;
+        accum += GET_WHEEL_DELTA_WPARAM(wParam);
+        if (abs(accum) >= WHEEL_DELTA) {
+            POINT ptCursor = { GET_X_LPARAM(lParam) , GET_Y_LPARAM(lParam) };
+            int speed = 4;
+            GrowShrink((accum > 0) ? speed : -speed, ptCursor,
+                !!(GET_KEYSTATE_WPARAM(wParam) & MK_CONTROL),
+                !!(GET_KEYSTATE_WPARAM(wParam) & MK_SHIFT));
+            accum = 0;
         }
         break;
-
+    }
+    
     case WM_LBUTTONUP:
         if (!bHoverCloseButton) {
             break;
@@ -239,7 +239,7 @@ bool InitWindow()
         szWndClass,
         szWndTitle,
         WS_POPUPWINDOW,
-        500,500, defCX, defCY,
+        defX, defY, defCX, defCY,
         NULL, nullptr, hInst, nullptr);
 
     if (!hwnd) {
@@ -261,6 +261,7 @@ void LLNewPos(POINT pt)
     if (ptPrev.x != 0 || ptPrev.y != 0) {
         double delta = DIST(ptPrev, pt);
         distance += delta;
+        maxDelta = max(maxDelta, delta);
     }
     ptPrev = pt;
 
